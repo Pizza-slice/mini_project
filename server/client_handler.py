0caa0332749f3233ba22882e9a46e77cd0d981a2
Pizza-slice <45051006+Pizza-slice@users.noreply.github.com>
2019-10-29 16:03:29 +0200

    Update client_handler.py

--- server/client_handler.py
+++ server/client_handler.py
@@ -1,11 +1,13 @@
 import threading
+import database
+import json
 
 
 class ClientHandler(threading.Thread):
-    def __init__(self, client_socket, server):
+    def __init__(self, client_socket):
         threading.Thread.__init__(self)
         self.client_socket = client_socket
-        self.server = server
+        self.database = database.AsycDatabase("database.db")
 
     def get_data_from_client(self):
         return self.client_socket.recv(1024).decode()
@@ -14,7 +16,15 @@ class ClientHandler(threading.Thread):
         self.client_socket.send(data.endcode())
 
     def run(self):
-        pass
+        data = self.get_data_from_client()
+        json_data = json.loads(data)
+        if json_data["type"] == "log-in":
+            success, massage = self.database.check_user(json_data["username"], json_data["password"])
+            if not success:
+                self.client_socket.send(json.dumps({"type": "failed", "massage": massage}))
+            else:
+                auth_code = self.database.get_auth_code(json_data["username"])
+                self.client_socket.send(json.dumps({"type": "success", "auth_code": auth_code}))
 
 
 def main():
